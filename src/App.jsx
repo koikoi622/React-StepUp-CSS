1f5135673f6d770ec506486d00c5a14a4518bfdd
koikoi622 <104248045+koikoi622@users.noreply.github.com>
2022-04-30 16:14:21 +0900

    04301614

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import React, { useCallback, useState, useMemo } from "react";
-import { ChildArea } from "./ChildArea";
+import React, { useState } from "react";
+import { CssModules } from "./components/CssModules";
+import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
+
 import "./styles.css";
 
 export const App = () => {
-  console.log("App");
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (event) => setText(event.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
-    <>
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </>
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+    </div>
   );
 };
